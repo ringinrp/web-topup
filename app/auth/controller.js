a86740cb5debe777db98927bcf6be833edcea5e5
ringinrp <ringinrp21@gmail.com>
2024-04-12 11:37:29 +0700

    update auth

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -4,14 +4,21 @@ module.exports = {
     signup: async (req, res, next) => {
         try {
             const payload = req.body
+
+            if(req.file){
+
+            }else{
+
+            }
             
             res.status(201).json({
                 message: payload
             })
-            
+
         } catch (err) {
             if (err && err.name === "ValidationError") {
                 return res.status(422).json({
+                    error: 1,
                     message: err.message,
                     fields: err.errors
                 })
